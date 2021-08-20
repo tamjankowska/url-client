5d4281dcd47c2357f968811505615a43445689fa
Tam <tam.jankowska@gmail.com>
2021-08-20 15:22:01 +0100

    basic material-ui components

--- src/App.js
+++ src/App.js
@@ -1,25 +1,34 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react'
+import Box from '@material-ui/core/Box'
+import TextField from '@material-ui/core/TextField'
+import Typography from '@material-ui/core/Typography'
+import Card from '@material-ui/core/Card'
+import CardContent from '@material-ui/core/CardContent'
+import Navbar from './components/Navbar.js'
 
-function App() {
+export default function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+    <>
+    <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Roboto:300,400,500,700&display=swap" />
+      <Navbar />
+      <Box
+        display="flex"
+        flexDirection="column"
+        justifyContent="center"
+        alignItems="center"
+      >
+        <TextField
+          style={{ padding: 20 }}
+          margin="dense"
+          variant="outlined"
+          id="inputUrl"
+          placeholder="Enter your url"
+          />
 
-export default App;
+        <Typography style={{ padding: 30 }}>
+          Your shortened url...
+        </Typography>
+      </Box>
+    </>
+  )
+}
